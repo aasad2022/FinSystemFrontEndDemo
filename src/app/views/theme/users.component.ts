5fb96515050dffa86207f2e6526256b80787c3fe
aasad <aasad@qou.edu>
2023-04-30 20:15:22 +0300

    users ...

--- src/app/views/theme/users.component.ts
+++ src/app/views/theme/users.component.ts
@@ -2,6 +2,7 @@ import {User} from "../../models/user";
 import {UserService} from "../../services/user-service.service";
 import {Component, OnInit} from '@angular/core';
 import DataTables from "datatables.net-dt";
+import LanguageSettings = DataTables.LanguageSettings;
 
 @Component({
   selector: 'app-users',
@@ -27,10 +28,33 @@ export class UsersComponent implements OnInit {
         console.log(data.content);
         this.users = data.content;
       }
-    )
-    ;
+    );
+
+    const language: LanguageSettings = {
+      emptyTable:     "لا توجد بيانات متوفرة",
+      info:           "عرض _START_ الى _END_ من _TOTAL_ مدخلة",
+      infoEmpty:      "عرض 0 الى 0 من 0 مدخلة",
+      infoFiltered:   "(تصفية من _MAX_ مجموع المدخلات)",
+      infoPostFix:    "",
+      lengthMenu:     "عرض _MENU_ مدخلة",
+      loadingRecords: "تحميل...",
+      processing:     "Processing...",
+      search:         "بحث:",
+      zeroRecords:    "لم يتم العثور على سجلات مطابقة",
+      paginate: {
+        first:    "الاول",
+        last:     "الاخير",
+        next:     "التالي",
+        previous: "السابق"
+      },
+      aria: {
+        sortAscending:  ": activate to sort column ascending",
+        sortDescending: ": activate to sort column descending"
+      }
+    };
     this.dtOptions = {
-      pagingType: 'full_numbers'
+      pagingType: 'full_numbers',
+      language: language
     };
   }
 }
